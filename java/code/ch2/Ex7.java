9f1e1f93620ac9a74de3a2f36c81f518539b4bc5
star-sil <kimse9811@naver.com>
2022-07-01 23:08:14 +0900

    ADD essential_java_chapter2 code

---
+++ java/code/ch2/Ex7.java
@@ -0,0 +1,12 @@
+package ch2;
+
+public class Ex7 {
+    public static void main(String[] args) {
+        char ch = 'A';
+        int code = (int)ch;
+
+        System.out.printf("%c=%d(%#X)%n", ch, code, code);
+        char hch = '가';
+        System.out.printf("%c=%d(%#X)%n", hch, (int) hch, (int) hch);
+    }
+}
